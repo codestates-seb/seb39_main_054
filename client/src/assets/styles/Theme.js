b4811365940f793901881a815eba24c7c26fb803
homile <ssanho2@naver.com>
2022-09-16 17:44:50 +0900

    chore: 색상 변수지정

--- client/src/assets/styles/Theme.js
+++ client/src/assets/styles/Theme.js
@@ -1,25 +1,42 @@
+const colors = {
+  gray1: "#333333",
+  gray2: "#4F4F4F",
+  gray3: "#828282",
+  gray4: "#BDBDBD",
+  gray5: "#E0E0E0",
+  gray6: "#F2F2F2",
+  stateGreen: "#4AFF2C",
+  stateBlue: "#0094FF",
+  stateRed: "#FF4646",
+};
+
 export const lightTheme = {
-  color: {
+  colors: {
+    // 변하는 부분
     primary: "#62BF53",
     bgColor: "#FFFFFF",
     textColor: "#000000",
-    textColor2: "#000000",
+    lightGreen: "#1FD203",
     black: "#000000",
     white: "#FFFFFF",
-    gray100: "#333333",
-    gray200: "#4F4F4F",
+    placholder: "#828282",
+
+    // 변하지 않는 부분
+    ...colors,
   },
 };
 
-export const darkTheme = { 
-  color: {
+export const darkTheme = {
+  colors: {
     primary: "#62BF53",
-    bgColor: "#1E1E22",
-    textColor: "#CCC",
+    bgColor: "#2F2F2F",
+    textColor: "#FFFFFF",
+    lightGreen: "#1FD203",
     black: "#000000",
     white: "#FFFFFF",
-    gray100: "#333333",
-    gray200: "#4F4F4F",
+    placholder: "#ffffff",
+
+    ...colors,
   },
 };
 

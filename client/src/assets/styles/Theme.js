254f25cce6452f7a203a7c024c69bd1fba390b29
homile <ssanho2@naver.com>
2022-09-16 16:46:43 +0900

    chore:폴더 초기 세팅

---
+++ client/src/assets/styles/Theme.js
@@ -0,0 +1,29 @@
+export const lightTheme = {
+  color: {
+    primary: "#62BF53",
+    bgColor: "#FFFFFF",
+    textColor: "#000000",
+    textColor2: "#000000",
+    black: "#000000",
+    white: "#FFFFFF",
+    gray100: "#333333",
+    gray200: "#4F4F4F",
+  },
+};
+
+export const darkTheme = { 
+  color: {
+    primary: "#62BF53",
+    bgColor: "#1E1E22",
+    textColor: "#CCC",
+    black: "#000000",
+    white: "#FFFFFF",
+    gray100: "#333333",
+    gray200: "#4F4F4F",
+  },
+};
+
+export const theme = {
+  lightTheme,
+  darkTheme,
+};

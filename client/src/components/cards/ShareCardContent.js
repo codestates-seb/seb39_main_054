d1a8bca1d463918e53cd6d653a4c73558fd03642
homile <ssanho2@naver.com>
2022-09-20 14:24:27 +0900

    feat:공유 물품 리스트 카드 추가

---
+++ client/src/components/cards/ShareCardContent.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import styled from 'styled-components';
+import ShareCard from './ShareCard';
+
+const ShareCardContent = ({data, number}) => {
+  return (
+    <CardContent>
+    {data !== null && data.map((el) => (
+      <ShareCard
+        id={el.productId}
+        title={el.title}
+        content={el.description}
+        status={el.status}
+        image01={el.image01}
+      />
+    )).slice(0, number)}
+  </CardContent>
+  )
+}
+
+export default ShareCardContent
+
+const CardContent = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+`;

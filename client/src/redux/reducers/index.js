b43a2b1cb20bab212a20f5e7d1099311fc143928
homile <ssanho2@naver.com>
2022-09-21 14:54:16 +0900

    feat:필터 리덕스 초기 세팅

--- client/src/redux/reducers/index.js
+++ client/src/redux/reducers/index.js
@@ -1,7 +1,9 @@
-import { combineReducers } from "redux" // 여러 reducer 들을 하나의 store에 저장 할 수 있게 해주는 함수
-import loginReducer from "./logInReducer"
+import { combineReducers } from "redux"; // 여러 reducer 들을 하나의 store에 저장 할 수 있게 해주는 함수
+import loginReducer from "./logInReducer";
+import filtersReducer from "./filtersReducer";
 
 const rootReducer = combineReducers({
   loginReducer,
-})
-export default rootReducer
+  filtersReducer,
+});
+export default rootReducer;

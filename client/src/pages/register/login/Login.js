6d5467600b3bfe6250621a12bf3b1ba55e41ebe3
crdrst777 <crdrst777@gmail.com>
2022-09-23 02:18:10 +0900

    feat: Login 기능 구현 1차 작업

--- client/src/pages/register/login/Login.js
+++ client/src/pages/register/login/Login.js
@@ -1,66 +1,165 @@
 import React, { useState } from "react";
+import { useDispatch } from "react-redux";
+import { loginSuccess } from "../../../redux/actions/logInAction";
 import styled from "styled-components";
+import axios from "axios";
+import Modal2 from "../../../components/ui/modals/Modal2";
+import { useNavigate } from "react-router-dom";
+import { useForm } from "react-hook-form";
+import { yupResolver } from "@hookform/resolvers/yup";
+import * as yup from "yup";
+import Validations from "../Validations";
 import logo_kakao from "../../../assets/img/sns/kakao_symbol.png";
 import logo_google from "../../../assets/img/sns/google_symbol.png";
 import logo_naver from "../../../assets/img/sns/naver_symbol.png";
-// import { Link } from "react-router-dom";
+
+const schema = yup.object().shape({
+  id: yup.string().required("아이디를 입력해주세요."),
+  password: yup.string().required("비밀번호를 입력해주세요."),
+});
 
 const Login = () => {
-  // const [email, setEmail] = useState("")
-  // const [password, setPassword] = useState("")
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+  const [isOpen, setIsOpen] = useState(false);
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({
+    resolver: yupResolver(schema),
+  });
+
+  const errSignup = () => {
+    // alert("사용자가 없습니다");
+    setIsOpen(!isOpen);
+  };
+
+  // 모달창의 확인버튼을 눌렀을때의 동작
+  const handleModal = () => {
+    setIsOpen(!isOpen);
+    // navigate(`/member`);
+  };
+
+  const onSubmit = async (data) => {
+    await axios
+      .get(`${process.env.REACT_APP_API_URL}/member`, {
+        // memberName: data.id,
+        // password: data.password,
+      })
+      .then((res) => {
+        let jwtToken = res.headers.authorization;
+        const memberid = res.headers.memberid;
+        localStorage.setItem("authorization", jwtToken);
+        localStorage.setItem("memberid", memberid);
+        dispatch(loginSuccess(memberid));
+        console.log("ok");
+        // navigate(`/`);
+        console.log(res);
+      })
+      .catch((err) => {
+        if (err.message === "Request failed with status code 500") {
+          // alert("사용자가 없습니다");
+          errSignup();
+          setIsOpen(!isOpen);
+        }
+        console.log(err);
+      });
+  };
 
   return (
     <>
       <LoginContainer>
         <h1>ANBD</h1>
         <LoginContent>
-          <Form>
-            <label>아이디</label>
-            <Input
-              mb="4.625rem"
-              type="text"
-              placeholder="아이디를 입력해주세요"
+          <form onSubmit={handleSubmit(onSubmit)}>
+            <div style={{ marginBottom: "4.2rem" }}>
+              <label>아이디</label>
+              <input
+                type="text"
+                placeholder="아이디를 입력해주세요"
+                {...register("id")}
+              />
+              {errors.id && <Validations value={errors.id.message} />}
+            </div>
+            <div style={{ marginBottom: "3.5rem" }}>
+              <label>비밀번호</label>
+              <input
+                type="password"
+                placeholder="비밀번호를 입력해주세요"
+                {...register("password")}
+              />
+              {errors.password && (
+                <Validations value={errors.password.message} />
+              )}
+            </div>
+            <button type="submit">로그인</button>
+            <Modal2
+              isOpen={isOpen}
+              setIsOpen={setIsOpen}
+              handleModal={handleModal}
+              children={"사용자가 없습니다"}
             />
-            <label>비밀번호</label>
-            <Input
-              mb="3.5rem"
-              type="password"
-              placeholder="비밀번호를 입력해주세요"
-            />
-            <button>로그인</button>
-          </Form>
-          <Divider>
+          </form>
+          <p className="divider">
             <span>소셜 로그인</span>
-          </Divider>
-          <Social>
-            <KakaoButton>
-              <LogoImage width="2.1rem" src={logo_kakao}></LogoImage>
-            </KakaoButton>
-            <GoogleButton>
-              <LogoImage width="2rem" src={logo_google}></LogoImage>
-            </GoogleButton>
-            <NaverButton>
-              <LogoImage width="2.7rem" src={logo_naver}></LogoImage>
-            </NaverButton>
-          </Social>
+          </p>
+          <div className="social">
+            <button
+              className="social-button"
+              style={{ backgroundColor: "#ffe600" }}
+            >
+              <img
+                className="logo-image"
+                style={{ width: "2.1rem" }}
+                src={logo_kakao}
+                alt="logo_kakao"
+              ></img>
+            </button>
+            <button
+              className="social-button"
+              style={{ backgroundColor: "#ffffff" }}
+            >
+              <img
+                className="logo-image"
+                style={{ width: "2rem" }}
+                src={logo_google}
+                alt="logo_google"
+              ></img>
+            </button>
+            <button
+              className="social-button"
+              style={{ backgroundColor: "#09c858" }}
+            >
+              <img
+                className="logo-image"
+                style={{ width: "2.5rem" }}
+                src={logo_naver}
+                alt="logo_naver"
+              ></img>
+            </button>
+          </div>
         </LoginContent>
       </LoginContainer>
     </>
   );
 };
+
 export default Login;
 
 const LoginContainer = styled.div`
   background-color: ${(props) => props.theme.bgColor};
   color: ${(props) => props.theme.textColor};
-  height: 100vh;
+  height: 100%;
   display: flex;
   flex-direction: column;
   justify-content: center;
 
   h1 {
     text-align: center;
-    margin: 8.125rem 0 3.4375rem 0;
+    /* margin: 8.125rem 0 3.4375rem 0; */
+    margin: 3.8rem 0 1.3125rem 0;
     font-size: 2.9375rem;
     font-family: "NotoSansKR-Bold";
   }
@@ -76,12 +175,32 @@ const LoginContent = styled.div`
   height: 40.625rem;
   margin: 0 auto 8.875rem auto;
   /* box-shadow: 0 0 10px gray; */
-`;
 
-const Form = styled.div`
   label {
     font-size: 1.5rem;
     padding: 0 0 0 0.625rem;
+  }
+
+  input {
+    width: 28.125rem;
+    height: 3.75rem;
+    background-color: ${(props) => props.theme.bgColor};
+    color: ${(props) => props.theme.textColor};
+    border: 0.08rem solid ${(props) => props.theme.gray4};
+    border-radius: 0.625rem;
+    font-size: 1.25rem;
+    padding-left: 1.25rem;
+    margin-top: 0.6875rem;
+    margin-bottom: ${(props) => props.mb};
+    ::placeholder,
+    ::-webkit-input-placeholder {
+      color: ${(props) => props.theme.gray4};
+    }
+    &:focus {
+      border: 0.09rem solid;
+      border-color: ${(props) => props.theme.primary};
+      outline: none;
+    }
   }
 
   button {
@@ -95,75 +214,34 @@ const Form = styled.div`
       filter: drop-shadow(0rem 0.25rem 0.25rem ${(props) => props.theme.gray5});
     }
   }
-`;
 
-const Input = styled.input`
-  width: 28.125rem;
-  height: 3.75rem;
-  background-color: ${(props) => props.theme.bgColor};
-  color: ${(props) => props.theme.textColor};
-  border: 0.08rem solid ${(props) => props.theme.gray4};
-  border-radius: 0.625rem;
-  font-size: 1.25rem;
-  padding-left: 1.25rem;
-  margin-top: 0.6875rem;
-  margin-bottom: ${(props) => props.mb};
-  ::placeholder,
-  ::-webkit-input-placeholder {
-    color: ${(props) => props.theme.gray4};
-  }
-  &:focus {
-    border: 0.09rem solid;
-    border-color: ${(props) => props.theme.primary};
-    outline: none;
-  }
-`;
+  .divider {
+    width: 100%;
+    text-align: center;
+    border-bottom: 0.1875rem solid ${(props) => props.theme.primary};
+    line-height: 0.1rem;
+    margin: 4.6875rem 0;
 
-const Divider = styled.p`
-  width: 100%;
-  text-align: center;
-  border-bottom: 0.1875rem solid ${(props) => props.theme.primary};
-  line-height: 0.1rem;
-  margin: 4.6875rem 0;
-
-  span {
-    font-size: 1rem;
-    padding: 0 1.5rem;
-    background-color: ${(props) => props.theme.bgColor};
+    span {
+      font-size: 1rem;
+      padding: 0 1.5rem;
+      background-color: ${(props) => props.theme.bgColor};
+    }
   }
-`;
 
-const Social = styled.div`
-  display: flex;
-  justify-content: space-between;
-  width: 90%;
-`;
+  .social {
+    display: flex;
+    justify-content: space-between;
+    width: 90%;
+  }
 
-const SocialButton = styled.button`
-  width: 3.75rem;
-  height: 3.75rem;
-  border-radius: 50%;
-  border: 0.03125rem solid ${(props) => props.theme.gray4};
-`;
-const KakaoButton = styled(SocialButton)`
-  background-color: #ffe600;
-  :hover {
-    filter: drop-shadow(0rem 0.25rem 0.25rem ${(props) => props.theme.gray5});
+  .social-button {
+    width: 3.75rem;
+    height: 3.75rem;
+    border-radius: 50%;
+    border: 0.03125rem solid ${(props) => props.theme.gray4};
+    :hover {
+      filter: drop-shadow(0rem 0.25rem 0.25rem ${(props) => props.theme.gray5});
+    }
   }
-`;
-const GoogleButton = styled(SocialButton)`
-  background-color: #ffffff;
-  :hover {
-    filter: drop-shadow(0rem 0.25rem 0.25rem ${(props) => props.theme.gray5});
-  }
-`;
-const NaverButton = styled(SocialButton)`
-  background-color: #09c858;
-  :hover {
-    filter: drop-shadow(0rem 0.25rem 0.25rem ${(props) => props.theme.gray5});
-  }
-`;
-
-const LogoImage = styled.img`
-  width: ${(props) => props.width};
 `;

038e9b435fa2d16fba42d2058c5f590767deadcf
jinwapp <jinwapp@gmail.com>
2022-09-28 21:47:39 +0900

    feat: product 수정, 삭제 구현 완료

---
+++ server/src/main/java/com/codestates/product/dto/ProductPatchDetailDto.java
@@ -0,0 +1,20 @@
+package com.codestates.product.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.validation.constraints.NotBlank;
+
+@Getter
+@Setter
+public class ProductPatchDetailDto {
+
+    private Long productId;
+    private Long memberId;
+    @NotBlank(message = "제목을 입력해주세요")
+    private String title;
+    @NotBlank(message = "내용을 입력해주세요")
+    private String description;
+    private String pcategoryName;
+
+}

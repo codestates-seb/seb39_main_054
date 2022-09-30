5997cf7e77454b9649e96d0d03624f10711a14af
crdrst777 <crdrst777@gmail.com>
2022-09-30 11:14:35 +0900

    chat 작업

--- client/src/pages/chat/chatDetail/ChatDetail.js
+++ client/src/pages/chat/chatDetail/ChatDetail.js
@@ -4,6 +4,7 @@
 // import * as StompJs from "@stomp/stompjs";
 
 // const ChatDetail = () => {
+//   const [content, setContent] = useState("");
 //   const Stomp = require("stompjs"); // --> ??
 
 //   let sock = new SockJS("http://localhost:8080/chat/detail");
@@ -15,9 +16,9 @@
 //     // 1. 서버와 연결
 //     client.connect({}, () => {
 //       console.log("Connected : " + memberid);
-//       // 2.접속한 유저의 정보를 서버로 보내기. --> 이거 필요한 과정이겠지???
+//       // 2.접속한 유저의 정보를 서버로 보내기.
 //       client.send("/app/join", {}, JSON.stringify(memberid));
-//       // 유저가 로그인하면(접속하면) 접속한 유저의 아이디를 서버로 보내서 접속한 유저의 정보를 받게 된다.
+//       // 유저가 로그인하면(접속하면) 접속한 유저의 아이디를 서버로 보내고 서버는 접속한 유저의 정보를 받게 된다.
 
 //       // 3.메세지 보내기
 //       client.send(
@@ -25,16 +26,26 @@
 //         {},
 //         JSON.stringify("Hello!")
 //       );
-//       // ${메세지 받을 대상} / 서버는 아이디를 받고 아이디를 가진 유저에게 다시 메세지 내용을 보낸다.
-//       // client.send(`/app/chat/보낼주소`,{},JSON.stringify(보낼데이터))
+//       // 서버는 메세지 받을 대상의 아이디를 받고 이 아이디를 가진 유저에게 메세지를 전달한다.
+//       // client.send(`/app/chat/보낼주소`,{헤더(필수 아님)},JSON.stringify(보낼데이터))
 
 //       // 클라이언트(메세지와 받을사람아이디를 같이 전송) -> 서버(메세지를 받을 사람에게 전달) -> 클라이언트(받을사람이 메세지 수신)
 
 //       // 4. 구독
-//       client.subscribe("/queue/addChatToClient/" + memberid, function (data) {
-//         const newMessage = JSON.parse(data.body);
-//       });
+//       client
+//         .subscribe(
+//           "/queue/addChatToClient/" + memberid,
+//           (data) => {
+//             const newMessage = JSON.parse(data.body);
+//             setContent(newMessage);
+//           },
+//           {}
+//         )
+//         .unsubscribe();
 //     });
+//     // 위의 콜백함수는 클라이언트가 서버로부터 메세지(data)를 전달받을때 실행된다.
+//     // 특정 채팅방에서 나가거나 해당 어플리케이션에서 나갔을 경우 상대방이 보낸 메세지를 읽지 않음으로 표시하기 위해 구독을 끊어준다.
+//     // 전달하는 메세지 / 전달받는 메세지를 따로 변수에 담아준다. --> 그래야 css 작업할 수 있을듯.
 
 //     return () => client.disconnect();
 //   }, [client, memberid, dispatch]); // <--- ???
@@ -44,7 +55,7 @@
 //       <CDContainer>
 //         <h1>채팅</h1>
 //         <CDContent>
-//           <div>{newMessage}</div>
+//           <div>{content}</div>
 //         </CDContent>
 //       </CDContainer>
 //     </>

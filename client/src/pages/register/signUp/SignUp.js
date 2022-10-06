296ebd9a8154274df7644b9d6c9c30aab501b32f
crdrst777 <crdrst777@gmail.com>
2022-10-07 01:42:29 +0900

    feat: 실시간 유효성 검사 기능 구현

--- client/src/pages/register/signUp/SignUp.js
+++ client/src/pages/register/signUp/SignUp.js
@@ -48,6 +48,7 @@ const SignUp = () => {
     formState: { errors },
   } = useForm({
     resolver: yupResolver(schema),
+    mode: "onChange",
   });
 
   const duplicateCheck = () => {
@@ -86,11 +87,15 @@ const SignUp = () => {
       });
   };
 
+  const onError = (error) => {
+    console.log(error);
+  };
+
   return (
     <SignupContainer>
       <h1>회원가입</h1>
       <SignupContent>
-        <form onSubmit={handleSubmit(onSubmit)}>
+        <form onSubmit={handleSubmit(onSubmit, onError)}>
           <div style={{ marginBottom: "1.05rem" }}>
             <label>아이디</label>
             <input
